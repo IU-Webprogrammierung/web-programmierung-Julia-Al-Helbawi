8fb78206eee447634780465ba472d40fc56cc0b1
Julia Al-Helbawi <julia.alhelbawi@iubh-fernstudium.de>
2025-03-03 13:09:19 +0100

    Style-Tags in html entfernt, Scroll-to-Top-Button korrigiert, Funktion hinzugefügt.

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-function toggleMenu() {
-    const sidebar = document.querySelector('.sidebar');
-    sidebar.classList.toggle('active');
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
 }
 
-
+window.onscroll = function () {
+  const button = document.querySelector('.scroll-to-top');
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    button.style.display = 'block'; // Button anzeigen
+  } else {
+    button.style.display = 'none'; // Button verstecken
+  }
+};
