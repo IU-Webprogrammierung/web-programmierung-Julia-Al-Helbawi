436ae39af342aefa3ead2f037de91b682de92e7a
Julia Al-Helbawi <julia.alhelbawi@iubh-fernstudium.de>
2025-03-13 18:33:50 +0100

    Dark Mode hinzugefügt.

--- script.js
+++ script.js
@@ -9,6 +9,31 @@ function toggleMenu() {
   menuIcon.textContent = sidebar.classList.contains('open') ? 'x' : '☰'; 
 }
 
+function toggleDarkMode() {
+  const body = document.body;
+  const darkModeButton = document.querySelector('.dark-mode-toggle');
+
+  body.classList.toggle('dark-mode');
+
+  if (body.classList.contains('dark-mode')) {
+    localStorage.setItem('darkMode', 'enabled');
+    darkModeButton.innerHTML = '☀️ <span>Light Mode</span>';
+  } else {
+    localStorage.setItem('darkMode', 'disabled');
+    darkModeButton.innerHTML = '🌙 <span>Dark Mode</span>';
+  }
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const darkModeButton = document.querySelector('.dark-mode-toggle');
+  if (localStorage.getItem('darkMode') === 'enabled') {
+    document.body.classList.add('dark-mode');
+    darkModeButton.innerHTML = '☀️ <span>Light Mode</span>';
+  } else {
+    darkModeButton.innerHTML = '🌙 <span>Dark Mode</span>';
+  }
+});
 
 
 function scrollToTop() {
